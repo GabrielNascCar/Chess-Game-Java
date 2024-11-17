6a9ee9a0d29a76efce3266b7f29404a3868532c8
Gabriel do nascimento carvalho <nascimentocarvalho894@gmail.com>
2024-11-17 17:53:03 -0300

    Possible moves of a piece

--- src/main/java/chess/ChessMatch.java
+++ src/main/java/chess/ChessMatch.java
@@ -44,6 +44,9 @@ public class ChessMatch {
         if(!board.thereIsApiece(position)){
             throw new ChessException("There is no piece on source position");
         }
+        if(!board.piece(position).isTherreAnyPossibleMove()){
+            throw new ChessException("Therre is no possible moves for the shosen piece");
+        }
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece){
